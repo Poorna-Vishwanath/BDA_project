2e54280d10018c0127922e9cf350bb456b9d7b1d
Poorna-Vishwanath <53818189+Poorna-Vishwanath@users.noreply.github.com>
2020-12-08 20:59:23 +0530

    Create employeeController.js

---
+++ Node.js-Express-MongoDB-CRUD/project/controllers/employeeController.js
@@ -0,0 +1,112 @@
+const express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose');
+const Employee = mongoose.model('Employee');
+
+router.get('/', (req, res) => {
+    res.render("employee/addOrEdit", {
+        viewTitle: "Insert Climate Details"
+    });
+});
+
+router.post('/', (req, res) => {
+    if (req.body._id == '')
+        insertRecord(req, res);
+        else
+        updateRecord(req, res);
+});
+
+
+function insertRecord(req, res) {
+    var employee = new Employee();
+    employee.date = req.body.date;
+    employee.temperature = req.body.temperature;
+    employee.country = req.body.country;
+    employee.city = req.body.city;
+    employee.longitude = req.body.longitude;
+    employee.latitude= req.body.latitude;
+    employee.save((err, doc) => {
+        if (!err)
+            res.redirect('employee/list');
+        else {
+            if (err.name == 'ValidationError') {
+                handleValidationError(err, req.body);
+                res.render("employee/addOrEdit", {
+                    viewTitle: "Insert details",
+                    employee: req.body
+                });
+            }
+            else
+                console.log('Error during record insertion : ' + err);
+        }
+    });
+}
+
+function updateRecord(req, res) {
+    Employee.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+        if (!err) { res.redirect('employee/list'); }
+        else {
+            if (err.name == 'ValidationError') {
+                handleValidationError(err, req.body);
+                res.render("employee/addOrEdit", {
+                    viewTitle: 'Update Details',
+                    employee: req.body
+                });
+            }
+            else
+                console.log('Error during record update : ' + err);
+        }
+    });
+}
+
+
+router.get('/list', (req, res) => {
+    Employee.find((err, docs) => {
+        if (!err) {
+            res.render("employee/list", {
+                list: docs
+            });
+        }
+        else {
+            console.log('Error in retrieving employee list :' + err);
+        }
+    });
+});
+
+
+//function handleValidationError(err, body) {
+  //  for (field in err.errors) {
+    //    switch (err.errors[field].path) {
+      //      case 'date':
+        //        body['fullNameError'] = err.errors[field].message;
+          //      break;
+            //case 'email':
+              //  body['emailError'] = err.errors[field].message;
+                //break;
+            //default:
+              //  break;
+        //}
+    //}
+//}
+
+router.get('/:id', (req, res) => {
+    Employee.findById(req.params.id, (err, doc) => {
+        if (!err) {
+            res.render("employee/addOrEdit", {
+                viewTitle: "Update details",
+                employee: doc
+            });
+        }
+    });
+});
+
+router.get('/delete/:id', (req, res) => {
+    Employee.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) {
+            res.redirect('/employee/list');
+        }
+        else { console.log('Error in  deleting :' + err); }
+    });
+});
+
+module.exports = router;
